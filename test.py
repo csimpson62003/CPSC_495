c31fa21206755d798b8628477d7722364c4433cf
Caleb Simpson <csimpson1@muskingum.edu>
2025-11-07 15:13:04 -0500

    Updated Seperate Files

--- test.py
+++
@@ -1,5 +0,0 @@
-import torch
-print("PyTorch version:", torch.__version__)
-print("CUDA runtime:", torch.version.cuda)
-print("GPU available:", torch.cuda.is_available())
-print("GPU name:", torch.cuda.get_device_name(0))
